00bdac482dbb3e877863c16e64d94a5da7dea474
viv <viv.devel@gmail.com>
2025-12-01 20:35:29 +0900

    fix: explicit environment variable setting and rename CI env to ci-test

--- frontend/src/utils/environment.ts
+++ frontend/src/utils/environment.ts
@@ -4,6 +4,10 @@ export const getHeaderColor = (environment?: string) => {
       return "bg-gray-800";
     case "staging":
       return "bg-yellow-700";
+    case "preview":
+      return "bg-purple-700";
+    case "ci-test":
+      return "bg-green-700";
     case "local":
       return "bg-blue-700";
     default:
