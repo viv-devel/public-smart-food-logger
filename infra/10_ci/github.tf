ce4c837c185dcbbe04f86da37ff3d01c1b18ed8c
viv <viv.devel@gmail.com>
2025-12-14 17:40:55 +0900

    refactor: define explicit variables in modules instead of dynamic map

--- infra/10_ci/github.tf
+++ infra/10_ci/github.tf
@@ -1,7 +1,23 @@
 # Generated GitHub Environment Config for CI
 
+locals {
+  secrets = {
+    FITBIT_CLIENT_ID               = var.FITBIT_CLIENT_ID
+    FITBIT_CLIENT_SECRET           = var.FITBIT_CLIENT_SECRET
+    NEXT_PUBLIC_RECAPTCHA_SITE_KEY = var.NEXT_PUBLIC_RECAPTCHA_SITE_KEY
+  }
+  variables = {
+    APP_ENVIRONMENT                          = var.APP_ENVIRONMENT
+    FITBIT_BACKEND_REDIRECT_URI              = var.FITBIT_BACKEND_REDIRECT_URI
+    FITBIT_REDIRECT_URI                      = var.FITBIT_REDIRECT_URI
+    NEXT_PUBLIC_FITBIT_BACKEND_REDIRECT_URI  = var.NEXT_PUBLIC_FITBIT_BACKEND_REDIRECT_URI
+    NEXT_PUBLIC_FITBIT_FRONTEND_REDIRECT_URI = var.NEXT_PUBLIC_FITBIT_FRONTEND_REDIRECT_URI
+    NEXT_PUBLIC_MOCK_AUTH                    = var.NEXT_PUBLIC_MOCK_AUTH
+  }
+}
+
 resource "github_actions_environment_secret" "secrets" {
-  for_each = var.input_secrets
+  for_each = local.secrets
 
   repository      = "public-smart-food-logger"
   environment     = "ci"
@@ -10,7 +26,7 @@ resource "github_actions_environment_secret" "secrets" {
 }
 
 resource "github_actions_environment_variable" "vars" {
-  for_each = var.input_variables
+  for_each = local.variables
 
   repository    = "public-smart-food-logger"
   environment   = "ci"
