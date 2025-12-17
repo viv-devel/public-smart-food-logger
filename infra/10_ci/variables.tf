36941971f48f542aefda0b130910941303a4aadc
viv <viv.devel@gmail.com>
2025-12-17 23:07:11 +0900

    feat(infra): add reCAPTCHA v3 environment variables
    Added NEXT_PUBLIC_RECAPTCHA_V3_SITE_KEY to Netlify and CI.
    
    Added RECAPTCHA_V3_SECRET_KEY to GitHub CI, Staging, and Production secrets.

--- infra/10_ci/variables.tf
+++ infra/10_ci/variables.tf
@@ -12,3 +12,5 @@ variable "OAUTH_FITBIT_REDIRECT_URI"                { type = string }
 variable "FITBIT_CLIENT_ID"               { type = string }
 variable "FITBIT_CLIENT_SECRET"           { type = string }
 variable "NEXT_PUBLIC_RECAPTCHA_SITE_KEY" { type = string }
+variable "NEXT_PUBLIC_RECAPTCHA_V3_SITE_KEY" { type = string }
+variable "RECAPTCHA_V3_SECRET_KEY" { type = string }
